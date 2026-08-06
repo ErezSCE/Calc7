3ddc2f860ecfa058cfa9ce524acf7002dfb50913
ErezKon <erezkon@gmail.com>
2026-08-06 06:55:53 +0300

    [calc7]-[US-005]-test: add TextEncoder polyfill and adjust jest config

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,5 @@
-import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+// Polyfill globals for environments where they are missing (e.g., jsdom)
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
